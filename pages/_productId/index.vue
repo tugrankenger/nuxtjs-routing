30fcbc5cd144d278fdfb5af59dece554f0d79632
tugrankenger <turankenger@hotmail.com.tr>
2023-01-16 20:48:25 +0300

    initialized files and create page for id params

---
+++ pages/_productId/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+      <p>id info: {{ $route.params.productId }}</p>
+  </div>
+</template>
+
+<script>
+  
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      <p>test component</p>
+  </div>
+</template>
